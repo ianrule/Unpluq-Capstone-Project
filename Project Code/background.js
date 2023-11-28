0e75f1f1d0e606d92cb86058f724f095631ca37d
Biswas Gauli <82783166+biga6132@users.noreply.github.com>
2023-11-27 19:04:48 -0700

    blocking functionality
    finished blocking functionality for specific websites

---
+++ Project Code/background.js
@@ -0,0 +1,6 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tab.url && (tab.url.includes("facebook.com") || tab.url.includes("youtube.com") ||
+        tab.url.includes("tiktok.com") || tab.url.includes("instagram.com"))) {
+      chrome.tabs.update(tabId, {muted: true});
+    }
+  });
