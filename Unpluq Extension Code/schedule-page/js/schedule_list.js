ef1ee6507182c9a9ae73d144cea9c9e86d8c5b38
Ian Rule <rule.ian@gmail.com>
2023-12-02 23:01:59 -0700

    Finished for now!
    - Got rid of scss files so they don't confuse anyone on the team without SASS experience
    - Moved around and combined files from WP1 (coding work from the first sprint)
     - Combined all of the different manifest.json files
     - Moved Biswas's javascript files into the blocking-functionality folder and renamed them (for reference)
     - Moved Kaleb's schedule creation html file into the schedule-page folder and renamed it (for reference)
     - Moved Nikita's block.js file into the schedule-page js folder, renamed it, and began to bug fix on it
    - Started to clean up and add to the html files and the css file (had to cut this short; ran out of time)
    - Made a few more edits that I didn't document here, but I am happy to discuss in the next meeting (have to get to bed)
    - Got the extension icon working!

---
+++ Unpluq Extension Code/schedule-page/js/schedule_list.js
@@ -0,0 +1,45 @@
+// pulled from Nikita's "block.js" file
+
+let blockedWebsites = [];
+
+function addElement(myURL){
+    var blockedWebsite = document.createElement('div');
+    var blockedWebsiteP = document.createElement('p');
+    var blockedWebsiteI = document.createElement('input');
+
+
+    blockedWebsiteP.innerHTML = myURL;
+    blockedWebsiteP.classList.add("websiteItemName");
+    blockedWebsite.appendChild(blockedWebsiteP);
+
+    blockedWebsiteI.type = "button";
+    blockedWebsiteI.value = "X";
+    blockedWebsiteI.classList.add("websiteItemSwitch");
+    blockedWebsite.appendChild(blockedWebsiteI);
+
+    blockedWebsite.classList.add("websiteItem");
+    return blockedWebsite;
+}
+
+function loadData(){
+    for(i in blockedWebsites){
+        addElement(i);
+    }
+}
+
+function addToList(){
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const currentWebsite = new URL(tabs[0].url).hostname;
+
+        if (!blockedWebsites.includes(currentWebsite)) {
+            blockedWebsites.push(currentWebsite);
+            chrome.storage.sync.set({ blockedWebsites: blockedWebsites }, function () {
+                console.log(`${currentWebsite} has been added to the blocked list.`);
+            });
+            document.getElementById("blockList").appendChild(addElement(currentWebsite));
+        }
+    });
+}
+
+loadData(); //not sure why this isnt working rn
+document.getElementById('addWebsite').addEventListener('click', addToList);
